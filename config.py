394e0600665f52d575edf5345d7bc6fad79d7278
shin <vanvietkhtn@gmail.com>
2022-10-22 17:24:29 +0700

    change config

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'wowirdrift@gmail.com'
+    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ALGORITHM= os.environ.get('JWT_ALGORITHM') or 'HS256'
